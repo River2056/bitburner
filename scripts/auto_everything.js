fccf7d7cfbb36a083bb76ef7a14428cf914e10ca
Kevin_Tung__ <kevin.tung@hytechtech.com>
2023-04-25 16:56:23 +0800

    + add some logs

--- scripts/auto_everything.js
+++ scripts/auto_everything.js
@@ -5,24 +5,19 @@ export function checkForHackingPrograms(ns) {
     "ftpcrack.exe",
     "relaysmtp.exe",
     "httpworm.exe",
-    "sqlinject.exe"
+    "sqlinject.exe",
   ];
   let hasCount = 0;
 
-  if (ns.fileExists("brutessh.exe", "home"))
-    hasCount++;
+  if (ns.fileExists("brutessh.exe", "home")) hasCount++;
 
-  if (ns.fileExists("ftpcrack.exe", "home"))
-    hasCount++;
+  if (ns.fileExists("ftpcrack.exe", "home")) hasCount++;
 
-  if (ns.fileExists("relaysmtp.exe", "home"))
-    hasCount++;
+  if (ns.fileExists("relaysmtp.exe", "home")) hasCount++;
 
-  if (ns.fileExists("httpworm.exe", "home"))
-    hasCount++;
+  if (ns.fileExists("httpworm.exe", "home")) hasCount++;
 
-  if (ns.fileExists("sqlinject.exe", "home"))
-    hasCount++;
+  if (ns.fileExists("sqlinject.exe", "home")) hasCount++;
 }
 
 /** @param {import(".").NS} ns*/
@@ -35,27 +30,33 @@ export async function main(ns) {
     let restartCustomServerScript = false;
     const servers = ns.getPurchasedServers();
     if (servers.length !== customServers.length) {
+      ns.print("WARN new servers detected");
       restartCustomServerScript = true;
     } else {
-      servers.forEach(s => {
-        const oldServer = customServers.filter(cs => cs.hostname === s)[0];
+      servers.forEach((s) => {
+        const oldServer = customServers.filter((cs) => cs.hostname === s)[0];
         const newServer = ns.getServer(s);
-        if (oldServer.maxRam !== newServer.maxRam)
+        if (oldServer.maxRam !== newServer.maxRam) {
+          ns.print(`WARN server ram upgraded: ${newServer.hostname}`);
           restartCustomServerScript = true;
+        }
       });
     }
 
-    customServers = servers.map(s => ns.getServer(s));
+    customServers = servers.map((s) => ns.getServer(s));
     if (restartCustomServerScript) {
       if (ns.run("deploy_custom_scripts.js") !== 0)
-        ns.tprintf("rerun deploy_custom_scripts.js...");
+        ns.print("SUCCESS rerun deploy_custom_scripts.js...");
     }
 
     const currentPlayerLevel = ns.getHackingLevel();
     const currentHackingProgramsCount = checkForHackingPrograms(ns);
-    if (currentPlayerLevel !== playerHackLevel || currentHackingProgramsCount !== hasHackingProgramCount) {
-      ns.tprintf(`player hacking level difference detected`);
-      ns.tprintf(`rerun deploy_and_run.js...`);
+    if (
+      currentPlayerLevel !== playerHackLevel ||
+      currentHackingProgramsCount !== hasHackingProgramCount
+    ) {
+      ns.print(`WARN player hacking level difference detected`);
+      ns.print(`SUCCESS rerun deploy_and_run.js...`);
       ns.run("deploy_and_run.js");
     }
     playerHackLevel = currentPlayerLevel;
@@ -63,4 +64,3 @@ export async function main(ns) {
     await ns.sleep(10 * 60 * 1000);
   }
 }
-
