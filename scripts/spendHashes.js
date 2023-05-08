a08eb0a0af542da297f19be8974316c56bccbb24
Kevin_Tung__ <kevin.tung@hytechtech.com>
2023-05-09 00:18:41 +0800

    + modified for better user experience

--- scripts/spendHashes.js
+++ scripts/spendHashes.js
@@ -3,7 +3,11 @@ import { HOME } from "./constants";
 
 /** @param {import(".").NS} ns*/
 export async function main(ns) {
-  const args = ns.flags([["money", false]]);
+  ns.disableLog("sleep");
+  const args = ns.flags([
+    ["money", false],
+    ["loop", false],
+  ]);
 
   if (!args || !args.money) {
     ns.tprintf("money argument not specified, aborting...");
@@ -11,14 +15,33 @@ export async function main(ns) {
       "specifiy argument money to spend all hashes in exchange for maximum money"
     );
     ns.tprintf("> run spendHashes.js --money true");
+    ns.tprintf("specify --loop true => for continuous run of script");
     ns.tprintf(`total hashes in store: ${ns.hacknet.numHashes()}`);
     return;
   }
 
+  const TYPE = hacknetHashTarget("money");
   const totalHashes = ns.hacknet.numHashes();
-  const moneyCostHash = ns.hacknet.hashCost(hacknetHashTarget("money"));
-  if (totalHashes > moneyCostHash) {
-    const purchaseResult = ns.hacknet.spendHashes(hacknetHashTarget("money"), HOME, Math.floor(totalHashes / moneyCostHash));
-    ns.tprintf(purchaseResult ? "SUCCESS succeded in exchanging all hashed for money" : "ERROR exchange failed!");
+  const moneyCostHash = ns.hacknet.hashCost(TYPE);
+  while (true) {
+    if (totalHashes > moneyCostHash) {
+      const purchaseResult = ns.hacknet.spendHashes(
+        TYPE,
+        HOME,
+        Math.floor(totalHashes / moneyCostHash)
+      );
+      const msg = `SUCCESS succeded in exchanging all hashes for money: $${
+        Math.floor(totalHashes / moneyCostHash) * 1000000
+      }`;
+      if (purchaseResult) {
+        if (args && args.loop) ns.printf(msg);
+        else {
+          ns.tprintf(msg);
+          return;
+        }
+      }
+    }
+
+    await ns.sleep(1000);
   }
 }
